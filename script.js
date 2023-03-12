97f24647ae3729e46147f9e5c2fd7439d29f896f
mlgibson8 <michael.l.gibson@du.edu>
2023-03-11 20:01:45 -0700

    finished then redid java to make sense

--- script.js
+++ script.js
@@ -1,121 +1,61 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-var enter;
+var characterLength = 8;
+var choiceArr = [];
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
-var UPPERCASE_CHAR_CODES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var LOWERCASE_CHAR_CODES = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var NUMBER_CHAR_CODES = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var SPECIALCHARACTERS_CHAR_CODES = ["!", "#", "$", "%", "&", "'", "(", ")", "*","+", ",", "-", ".", "/", " <", "=", ">", "?", "@", "[","]", "^", "_", "`", "{", "|", "}",];
+var specialChararr = ["!", "#", "$", "%", "&", "'", "(", ")", "*","+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[","]", "^", "_", "`", "{", "|", "}",];
+var lowerCaseArr =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
-function arrayFromLowToHigh (low,high) {
-  const array = []
-  for (let i = low; i <= high; i++) {
-      array.push(i)        
-  }
-  return array
-}
-
-function generatePassword (){
-enter = prompt("How long would you like your code? ");
-if (!enter) {
-  alert("This needs a value");
-} else if (enter < 8 || enter > 128){
-  enter = prompt("Pick between 8 and 128");
-} else  {
-  confirmLowercase = confirm("Do you want lowercase?");
-  confirmNumber = confirm("Do you want numbers?");
-  confirmUppercase = confirm(" Do you want Uppercase?");
-  confirmCharacter = confirm("Do you want Special Characters?");
-};
-
-if(!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase){
-  choices = alert("you must choose at least one option");
-}
-else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase){
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(NUMBER_CHAR_CODES,LOWERCASE_CHAR_CODES,UPPERCASE_CHAR_CODES );
-} else if (confirmCharacter && confirmNumber && confirmUppercase) {
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(NUMBER_CHAR_CODES, UPPERCASE_CHAR_CODES);
-}
-else if (confirmCharacter && confirmNumber && confirmLowercase) {
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(NUMBER_CHAR_CODES, LOWERCASE_CHAR_CODES);
-}
-else if (confirmCharacter && confirmLowercase && confirmUppercase) {
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(LOWERCASE_CHAR_CODES, UPPERCASE_CHAR_CODES);
-}
-else if (confirmNumber && confirmLowercase && confirmUppercase) {
-  choices = NUMBER_CHAR_CODES.concat(LOWERCASE_CHAR_CODES, UPPERCASE_CHAR_CODES);
-}
-
-else if (confirmCharacter && confirmNumber) {
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(NUMBER_CHAR_CODES);
-
-} else if (confirmCharacter && confirmLowercase) {
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(LOWERCASE_CHAR_CODES);
-
-} else if (confirmCharacter && confirmUppercase) {
-  choices = SPECIALCHARACTERS_CHAR_CODES.concat(UPPERCASE_CHAR_CODES);
-}
-else if (confirmLowercase && confirmNumber) {
-  choices = LOWERCASE_CHAR_CODES.concat(NUMBER_CHAR_CODES);
-
-} else if (confirmLowercase && confirmUppercase) {
-  choices = LOWERCASE_CHAR_CODES.concat(UPPERCASE_CHAR_CODES);
-
-} else if (confirmNumber && confirmUppercase) {
-  choices = NUMBER_CHAR_CODESr.concat(UPPERCASE_CHAR_CODES);
-}
-
-else if (confirmCharacter) {
-  choices = SPECIALCHARACTERS_CHAR_CODES;
-}
-else if (confirmNumber) {
-  choices = NUMBER_CHAR_CODES;
-}
-else if (confirmLowercase) {
-  choices = LOWERCASE_CHAR_CODES;
-}
-
-else if (confirmUppercase) {
-  choices = space.concat(UPPERCASE_CHAR_CODES);
-};
-
-var password = [];
-
-for (var i = 0; i < enter; i++) {
-  var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-  password.push(pickChoices);
-}
-
-var ps = password.join("");
-    UserInput(ps);
-    return ps;
-}  
-
-function UserInput(ps){
-  document.getElementById("password").textContent = ps;
-}
-
-
-
+// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ var correctPrompts = getPrompts();
+ var passwordText = document.querySelector("#password");
+if (correctPrompts){ 
+  var freshpassword = generatePassword();
+  passwordText.value = freshpassword;
+ }
+}
 
-  passwordText.value = password;
-return password
+function generatePassword() {
+var password = "";
+for(var i = 0; i < characterLength; i++)
+{  var randomindex = Math.floor(Math.random() * choiceArr.length);
+  password = password + choiceArr[randomindex]; }
+
+  return password;
 }
 
 
+function getPrompts (){
+choiceArr = [];
 
+    characterLength = prompt("how long of a password do you want \r please keep it between 8 and 128 characters");
+    if(isNaN(characterLength) || characterLength <8 || characterLength > 128)
+    {
+        alert("invalid length or character, \r follow directions please")
+        return false;
+    }
+    
+     if (confirmLowercase = confirm("Do you want lowercase?")) 
+    {  choiceArr = choiceArr.concat(lowerCaseArr); }
+    if (confirmUppercase = confirm("Do you want uppercase?")) 
+    {  choiceArr = choiceArr.concat(upperCaseArr); }
+    if (confirmNumber = confirm("Do you want numbers?")) 
+    {  choiceArr = choiceArr.concat(numberArr); }
+    if (confirmCharacter = confirm("Do you want Special Characters?")) 
+    { choiceArr = choiceArr.concat(specialChararr); }      
+       
+    else if( !confirmLowercase && !confirmUppercase && !confirmNumber && !confirmCharacter)
+    { alert("you must choose at least one option \r do better");}
+     return true; 
 
-generateBtn.addEventListener("click", function () {
-  ps = generatePassword();
-  document.getElementById("password").placeholder = ps;
-});
-
+ }
